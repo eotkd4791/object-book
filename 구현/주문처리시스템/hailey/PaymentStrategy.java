74fbf89969fad59815022b8e4b41d135a0fe71f5
hwyi21 <58624211+hwyi21@users.noreply.github.com>
2024-12-29 21:46:10 +0900

    Update PaymentStrategy.java

--- 구현/주문처리시스템/hailey/PaymentStrategy.java
+++ 구현/주문처리시스템/hailey/PaymentStrategy.java
@@ -1,3 +1,4 @@
+// 10장 코드의 재사용 관점에서 결제 수단 별로 공통적으로 반복되는 pay 라는 기능의 interface 를 사용함으로 코드의 재사용을 줄임
 public interface PaymentStrategy {
     void pay(int amount);
     PaymentMethod getPaymentMethod();
