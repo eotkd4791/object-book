7295478f0e4df42761a8252ea5104f5ff69700d7
hwyi21 <58624211+hwyi21@users.noreply.github.com>
2024-12-29 21:27:22 +0900

    Create Order.java

---
+++ 구현/주문처리시스템/hailey/Order.java
@@ -0,0 +1,25 @@
+public class Order {
+    private ShoppingCart shoppingCart;
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        if (paymentMethod == PaymentMethod.CARD) {
+            paymentStrategy = new CardPaymentStrategy();
+        } else if (paymentMethod == PaymentMethod.BANK_TRANSFER) {
+            paymentStrategy = new BankTransferPaymentStrategy();
+        }  else if (paymentMethod == PaymentMethod.Mobile) {
+            paymentStrategy = new MobilePaymentStrategy();
+        }else {
+            throw new IllegalArgumentException("지원하지 않는 결제 방식입니다.");
+        }
+    }
+  
+    public void placeOrder() {
+        if (shoppingCart.isEmpty()) {
+            throw new EmptyShoppingCartException("장바구니가 비어있습니다.");
+        }
+
+        // 결제
+        paymentStrategy.pay(shoppingCart.getTotalPrice());
+    }
+}
