134b41c89e71aeae4a0c1077b99d10bdfab52026
hwyi21 <58624211+hwyi21@users.noreply.github.com>
2024-12-29 19:15:31 +0900

    Create cart.java

---
+++ 구현/주문처리시스템/hailey/cart.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private List<Product> items = new ArrayList<>();
+
+    public void addItem(Product product) {
+        if (product.getQuantity() > 0) {
+            items.add(product);
+        } else {
+            // 상품 수량이 0인 경우 예외 처
+        }
+    }
+
+    public void removeItem(Product product) {
+        items.remove(product);
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (Product product : items) {
+            totalPrice += product.getPrice() * product.getQuantity();
+        }
+        return totalPrice;
+    }
+}
