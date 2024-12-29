172fa804980ca3c355e2c14e35057fedde2f1dee
hwyi21 <58624211+hwyi21@users.noreply.github.com>
2024-12-29 21:47:36 +0900

    Update MobilePaymentStrategy.java

--- 구현/주문처리시스템/hailey/MobilePaymentStrategy.java
+++ 구현/주문처리시스템/hailey/MobilePaymentStrategy.java
@@ -1,5 +1,6 @@
 public class MobilePaymentStrategy implements PaymentStrategy {
     @Override
+    // pay 라는 공통 기능에대해 각 결제 수단별로 서로 다른 로직을 재공함으로써 확장성을 높임
     public void pay(int amount) {
         // 모바일 결제 로직
         System.out.println("모바일로 " + amount + "원 결제");
