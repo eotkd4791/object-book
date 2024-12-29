a3b756ba9a5d0f76e57cba4a49574d88ed5e1439
hwyi21 <58624211+hwyi21@users.noreply.github.com>
2024-12-29 19:21:12 +0900

    Update and rename cart.java to Cart.java

--- 구현/주문처리시스템/hailey/Cart.java
+++ 구현/주문처리시스템/hailey/Cart.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShoppingCart {
+public class Cart {
     private List<Product> items = new ArrayList<>();
 
     public void addItem(Product product) {
